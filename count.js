c914c89ef5b5327fb164d873bd9eebf449b84cd9
jorge hernandez <jehernandez@gmail.com>
2019-08-28 10:02:06 -0500

    creando funcion para contar palabras ..

--- count.js
+++ count.js
@@ -20,16 +20,16 @@ function convertNumberToWord(number)
     var c = ["", "ciento", "doscientos", "trescientos", "cuatrocientos", "quinientos", "seiscientos", "setecientos", "ochocientos", "novecientos"];
 
     var n = parseFloat(number).toFixed(2); //dos decimales
-    var p = n.toString().substring(n.toString().indexOf(".")+1); /decimales/
-    var m = n.toString().substring(0,n.toString().indexOf(".")); /número sin decimales/
+    var p = n.toString().substring(n.toString().indexOf(".")+1); //decimales/
+    var m = n.toString().substring(0,n.toString().indexOf(".")); //número sin decimales/
     var m = parseFloat(m).toString().split("").reverse(); 
     var t="";
 
-    /Se analiza cada 3 dígitos/
+    //Se analiza cada 3 dígitos/
   for (var i=0; i<m.length; i+=3){
 
     var x=t;
-    /Se forma un numero de tres/
+    //Se forma un numero de tres/
     var b= (m[i+1]!=undefined) ? parseFloat(m[i+1].toString()+m[i].toString()) : parseFloat(m[i].toString());
     t= (m[i+2]!=undefined) ? (c[m[i+2]]+" ") : "";
     t+= (b<10) ? u[b] :( (b<30) ? o[b-10] : (d[m[i+1]]+(m[i]=='0'?"":(" y "+u[m[i]]))) );
